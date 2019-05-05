27767833dc8fdf8c94305c7d15fb99d65e8c2a2d
Pavle Lekic <lekic.pavle@yahoo.com>
2019-05-05 15:08:07 +0200

    added random size to items

--- src/app.js
+++ src/app.js
@@ -3,31 +3,26 @@ import VirtualList from './virtual-list';
 
 const items = [];
 for (let i = 0; i < 1000; i++) {
-    items.push('Row #' + i);
+    items.push({
+        text: 'Row #' + i,
+        height: Math.floor(300 * Math.random()) + 50
+    });
 }
 
 class Item extends React.PureComponent {
-    static sharedStyle = {
-        paddingLeft: 12,
-        display: 'flex',
-        alignItems: 'center',
-        height: '100%'
-    };
-
-    static oddItemStyle = {
-        ...Item.sharedStyle,
-        backgroundColor: '#aaaaaa'
-    };
-
-    static evenItemStyle = {
-        ...Item.sharedStyle,
-        backgroundColor: '#cacaca'
-    };
-
     render() {
+        const { index } = this.props; 
+        const item = items[index];
+        const style = {
+            paddingLeft: 12,
+            display: 'flex',
+            alignItems: 'center',
+            backgroundColor: index % 2 === 0 ? '#cacaca' : '#aaaaaa',
+            height: item.height
+        }
         return (
-            <div style={this.props.index % 2 === 0 ? Item.evenItemStyle : Item.oddItemStyle}>
-                {items[this.props.index]}
+            <div style={style}>
+                {item.text}
             </div>
         );
     }
@@ -38,7 +33,6 @@ const App = () => {
         <VirtualList
             itemsCount={items.length}
             height={500}
-            itemHeight={50}
             ItemComponent={Item}
         />
     );
