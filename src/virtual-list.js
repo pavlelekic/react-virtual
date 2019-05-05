2afe5901e119752ed9a702cf3f71626734ab0e40
Pavle Lekic <lekic.pavle@yahoo.com>
2019-05-05 14:52:35 +0200

    some progress

--- src/virtual-list.js
+++ src/virtual-list.js
@@ -3,13 +3,26 @@ import PropTypes from 'prop-types';
 import memoize from 'memoize-one';
 import './virtual-list.scss';
 
-export default class VirtualList extends React.PureComponent {
+const UNDEFINED = -1;
+
+export default class AdaptiveVirtualList extends React.PureComponent {
   didScroll = false;
+  indexOfMeasuredItemsSoFar = 0;
 
   constructor(props) {
     super(props);
-    this.state = { scrollTop: 0 };
-
+    this.estimatedNumItemsOnScreen = Math.ceil(props.height / props.itemHeight);
+    const indexesOfItemsToCheckForHeight = [];
+    for (let i = 0; i < this.estimatedNumItemsOnScreen; i++) {
+      indexesOfItemsToCheckForHeight.push(i);
+    }
+    this.state = { scrollTop: 0, indexesOfItemsToCheckForHeight };
+    const { itemsCount } = props;
+    const itemHeights = [];
+    for (let i = 0; i < itemsCount; i++) {
+      itemHeights.push(UNDEFINED);
+    }
+    this.itemHeights = itemHeights;
     this.updateDom = this.updateDom.bind(this);
   }
 
@@ -18,9 +31,30 @@ export default class VirtualList extends React.PureComponent {
   }
 
   updateDom() {
+    // collect item heights and update itemHeights arr!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!11
+
+    
+    // determine which items to check for height
+    let startIndexOfItemsToCheckForHeight;
     if (this.didScroll) {
       this.setState({ scrollTop: this.listWrapperRef.scrollTop });
       this.didScroll = false;
+      // find approx next frame start index and set that for startIndexOfItemsToCheckForHeight, use polynomial approx 
+      startIndexOfItemsToCheckForHeight = this.itemHeights.indexOf(UNDEFINED, this.indexOfMeasuredItemsSoFar); // this is temporary
+    } else {
+      startIndexOfItemsToCheckForHeight = this.itemHeights.indexOf(UNDEFINED, this.indexOfMeasuredItemsSoFar);
+      // this is approx, indexOfMeasuredItemsSoFar could be greather than this
+      this.indexOfMeasuredItemsSoFar = Math.min(this.props.itemsCount - 1, this.indexOfMeasuredItemsSoFar + this.estimatedNumItemsOnScreen)
+    }
+    const indexesOfItemsToCheckForHeight = [];
+    for (let i = startIndexOfItemsToCheckForHeight; i < this.props.itemsCount || indexesOfItemsToCheckForHeight.length !== this.estimatedNumItemsOnScreen; i++) {
+      if (this.props.itemHeights[i] === UNDEFINED) {
+        indexesOfItemsToCheckForHeight.push(i);
+      }
+    }
+
+    if (indexesOfItemsToCheckForHeight.length > 0) {
+      this.setState({ indexesOfItemsToCheckForHeight });
     }
     this.rafHandle = window.requestAnimationFrame(this.updateDom);
   }
@@ -36,17 +70,17 @@ export default class VirtualList extends React.PureComponent {
 
   handleScroll = (event) => this.didScroll = true;
 
-  calcContentWrapperStyle = memoize((itemHeight, itemsCount) => ({
-    height: itemHeight * itemsCount
+  calcContentWrapperStyle = memoize((estimatedItemHeight, itemsCount) => ({
+    height: estimatedItemHeight * itemsCount
   }));
 
   saveListWrapperRef = (r) => this.listWrapperRef = r;
 
   renderVisibleItems() {
-    const { itemHeight, itemsCount, ItemComponent } = this.props;
-    const startIndex = Math.floor(this.state.scrollTop / itemHeight);
+    const { estimatedItemHeight, itemsCount, ItemComponent } = this.props;
+    const startIndex = Math.floor(this.state.scrollTop / estimatedItemHeight);
     const endIndex = Math.min(
-      startIndex + Math.ceil(this.props.height / itemHeight) + 1,
+      startIndex + this.estimatedNumItemsOnScreen + 1,
       itemsCount
     );
     const items = [];
@@ -55,22 +89,36 @@ export default class VirtualList extends React.PureComponent {
         <div
           key={i - startIndex}
           className="virtual-list__item-wrapper"
-          style={{ top: i * itemHeight, height: itemHeight}}
+          style={{ top: i * estimatedItemHeight, height: estimatedItemHeight}}
         >
           <ItemComponent index={i} />
         </div>
       );
     }
 
-    return (
-      <div
-        style={this.calcContentWrapperStyle(itemHeight, itemsCount)}
-        className="virtual-list__content-wrapper"
-      >
-        {items}
-      </div>
-    );
+    return items;
   }
+
+  renderItemsToFigureOutHeight() {
+    const { estimatedItemHeight, itemsCount, ItemComponent } = this.props;
+    const style = { top: 0 };
+    for (let i = 0; i < this.state.indexesOfItemsToCheckForHeight.length; i++) {
+      items.push(
+        <div
+          key={i}
+          className="virtual-list__item-wrapper virtual-list__item-wrapper--hidden"
+          style={style}
+          data-index={this.state.indexesOfItemsToCheckForHeight[i]}
+        >
+          <ItemComponent index={this.state.indexesOfItemsToCheckForHeight[i]} />
+        </div>
+      );
+    }
+
+    return items;
+  }
+
+  // saveHiddenItemsRef = (r) => this.hiddenItemsRef = r;
 
   render() {
     return (
@@ -79,19 +127,28 @@ export default class VirtualList extends React.PureComponent {
         ref={this.saveListWrapperRef}
         style={this.calcListWrapperStyle(this.props.style, this.props.height)}
         onScroll={this.handleScroll}
-      >{this.renderVisibleItems()}</div>
+      >
+        <div
+          style={this.calcContentWrapperStyle(estimatedItemHeight, itemsCount)}
+          className="virtual-list__content-wrapper"
+        >
+          {this.renderVisibleItems()}
+          {this.renderItemsToFigureOutHeight()}
+        </div>
+      </div>
     );
   }
 }
 
-VirtualList.propTypes = {
+AdaptiveVirtualList.propTypes = {
   style: PropTypes.object,
-  height: PropTypes.number.isRequired,
-  itemHeight: PropTypes.number.isRequired,
+  height: PropTypes.number,
+  estimatedItemHeight: PropTypes.number.isRequired,
   ItemComponent: PropTypes.oneOfType([PropTypes.func, PropTypes.object]).isRequired,
   itemsCount: PropTypes.number.isRequired
 }
 
-VirtualList.defaultProps = {
-  style: {}
+AdaptiveVirtualList.defaultProps = {
+  style: {},
+  height: 500
 };
